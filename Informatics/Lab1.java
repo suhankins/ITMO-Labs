22acee038455bc687cb5d26e6c5f59c859dec98e
ObiArt <punishedbernadetta@niuitmo.ru>
2021-10-09 13:02:59 +0300

    Added a link to test codes, changed "Control bit" to "Parity bit"

--- Informatics/Lab1.java
+++ Informatics/Lab1.java
@@ -5,11 +5,11 @@ public class Lab1 {
 		Scanner scanner = new Scanner(System.in);
 		System.out.println("Enter the code");
 		/*
-		Test codes with no errors:
+		Test codes with no errors: https://en.wikipedia.org/wiki/Hamming(7,4)#All_codewords
 		*/
 		char[] code = scanner.nextLine().toCharArray();
 		/*
-		C - Control bit
+		C - Parity bit
 		I - Information bit
 
 		C C I C I I I
@@ -21,27 +21,27 @@ public class Lab1 {
 		C3       X X X X
 		*/
 		int errorBit = 0;
-		char[] controlBits = new char[3];
-		for (int i = 0; i < controlBits.length; i++){
-			controlBits[i] = '0';
+		char[] parityBits = new char[3];
+		for (int i = 0; i < parityBits.length; i++){
+			parityBits[i] = '0';
 			int n = (int)Math.pow(2, i);
-			//This cycle goes through every starting index of sets of bits this control bit overlaps
+			//This cycle goes through every starting index of sets of bits this parity bit overlaps
 			for (int j = n - 1; j < code.length; j += n * 2){
-				//This cycle goes through every bit in a set of bits this control bit overlaps
+				//This cycle goes through every bit in a set of bits this parity bit overlaps
 				for (int k = j; k < j + n; k++){
 					if (k != n - 1) {
-						controlBits[i] = controlBits[i] != code[k] ? '1' : '0';
+						parityBits[i] = parityBits[i] != code[k] ? '1' : '0';
 					}
 				}
 			}
-			if (code[n - 1] != controlBits[i]) {
+			if (code[n - 1] != parityBits[i]) {
 				errorBit += n;
 			}
 		}
 		if (errorBit != 0) {
-			System.out.printf("%nThere was an error in bit %d", errorBit);
+			System.out.printf("%nThere was an error in bit %d%n", errorBit);
 			code[errorBit - 1] = code[errorBit - 1] == '0' ? '1' : '0';
 		}
-		System.out.printf("%nThe code is: %c%c%c%c", code[2], code[4], code[5], code[6]);
+		System.out.printf("The code is: %c%c%c%c", code[2], code[4], code[5], code[6]);
 	}
 }
