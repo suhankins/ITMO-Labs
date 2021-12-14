63bbb5fb399753964734f1ffee3ab4d9aad1bf5c
Dimitri Sukhankin <90870844+suhankins@users.noreply.github.com>
2021-12-14 14:11:24 +0300

    informatics lab4

---
+++ Informatics/lab4Automation.py
@@ -0,0 +1,12 @@
+while True:
+    a = str(bin(int(input())))[2:]
+    a = "0" * (16 - len(a)) + a
+    x = []
+    for i in range(0, len(a)):
+        x.append(a[i])
+        print(i)
+        if (i % 4 == 3 and not i == 1):
+            x.append(".")
+            print(i, "<------")
+
+    print("\t".join(x))
