4c7bf63fb8d865af9d94bc5f28d8e4340ae1f0a4
ObiArt <punishedbernadetta@niuitmo.ru>
2022-02-28 22:37:35 +0300

    made comments shorter

--- Programming/lab4/src/Vehicles/Vehicle.java
+++ Programming/lab4/src/Vehicles/Vehicle.java
@@ -15,87 +15,47 @@ public class Vehicle implements Comparable<Vehicle> {
     private static int counter = 0;
 
     /**
-     * <p>
-     * Unique vehicle id
-     * </p>
-     * <p>
-     * * Generated automatically
-     * </p>
-     * <p>
-     * * Cannot be null
-     * </p>
-     * <p>
-     * * Must be more than 0
-     * </p>
+     * <p> Unique vehicle id </p>
+     * <p> * Generated automatically </p>
+     * <p> * Cannot be null </p>
+     * <p> * Must be more than 0 </p>
      */
     private int id;
     /**
-     * <p>
-     * Vehicle's name
-     * </p>
-     * <p>
-     * * Cannot be null
-     * </p>
-     * <p>
-     * * Cannot be an empty string
-     * </p>
+     * <p> Vehicle's name </p>
+     * <p> * Cannot be null </p>
+     * <p> * Cannot be an empty string </p>
      */
     private String name;
     /**
-     * <p>
-     * Vehicle coordinates
-     * </p>
-     * <p>
-     * * Cannot be null
-     * </p>
+     * <p> Vehicle coordinates </p>
+     * <p> * Cannot be null </p>
      */
     private Coordinates coordinates;
     /**
-     * <p>
-     * Vehicle creation date
-     * </p>
-     * <p>
-     * * Generated automatically
-     * </p>
-     * <p>
-     * * Cannot be null
-     * </p>
+     * <p> Vehicle creation date </p>
+     * <p> * Generated automatically </p>
+     * <p> * Cannot be null </p>
      */
     private java.time.LocalDate creationDate;
     /**
-     * <p>
-     * Vehicle engine power
-     * </p>
-     * <p>
-     * * Must be more than 0
-     * </p>
+     * <p> Vehicle engine power </p>
+     * <p> * Must be more than 0 </p>
      */
     private int enginePower;
     /**
-     * <p>
-     * Amount of wheels a vehicle has
-     * </p>
-     * <p>
-     * * Must be more than 0
-     * </p>
+     * <p> Amount of wheels a vehicle has </p>
+     * <p> * Must be more than 0 </p>
      */
     private int numberOfWheels;
     /**
-     * <p>
-     * Vehicle type
-     * </p>
-     * <p>
-     * * Cannot be null
-     * </p>
+     * <p> Vehicle type </p>
+     * <p> * Cannot be null </p>
      */
     private VehicleType type;
     /**
-     * <p>
-     * Vehicle fuel type
-     * </p>
-     * <p>
-     * * Cannot be null
-     * </p>
+     * <p> Vehicle fuel type </p>
+     * <p> * Cannot be null </p>
      */
     private FuelType fuelType;
 
